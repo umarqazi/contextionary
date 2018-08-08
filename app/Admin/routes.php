9e72fb3654b1c66a8ee181a1efc096a304c14d56
Haris Khan <haris.khan@gems.techverx.com>
2018-08-08 12:14:39 +0500

    Changes in routes

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,17 +1,18 @@
 <?php
 
 use Illuminate\Routing\Router;
-
-Admin::registerAuthRoutes();
-Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
-], function (Router $router) {
-    $router->get('/', 'HomeController@index');
-    $router->resource('auth/simple-users', 'UserController');
-    $router->resource('auth/simple-users-roles', 'RoleController');
-    $router->resource('auth/simple-users-permissions', 'PermissionController');
-    $router->resource('auth/packages', 'PackagesController');
-    $router->resource('auth/points', 'PointsController');
-});
+        Admin::registerAuthRoutes();
+        Route::group([
+            'prefix'        => config('admin.route.prefix'),
+            'namespace'     => config('admin.route.namespace'),
+            'middleware'    => config('admin.route.middleware'),
+        ], function (Router $router) {
+            $router->get('/', 'HomeController@index')->name('admin_login');
+            $router->resource('auth/simple-users', 'UserController');
+            $router->resource('auth/simple-users-roles', 'RoleController');
+            $router->resource('auth/simple-users-permissions', 'PermissionController');
+            $router->resource('auth/packages', 'PackagesController');
+            $router->resource('auth/points', 'PointsController');
+            $router->resource('auth/texts', 'MultiLangController')->only('store', 'index', 'update','destroy','create');
+            $router->resource('auth/fun-facts', 'FunFactsController');
+        });
--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+MultiLang::routeGroup(function($router) {
+  Route::group(['middleware' => ['locale']], function(){
+    Route::get('/', function () {
+      return view('index');
+    })->name('homescreen');
+    Auth::routes();
+    Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('profile',  'UsersController@profile')->name('profile');
+    Route::get('edit-profile',  'UsersController@edit')->name('edit profile');
+    Route::get('selectPlan/{token}',  'UsersController@selectPlan')->name('selectPlan');
+    Route::get('userPlan/{id}/{token}',  'UsersController@userPlan')->name('userPlan');
+    Route::get('payment/{id}/{plan}/{token}',  'UsersController@showPaymentInfo')->name('payment');
+    Route::post('addmoney/stripe', array('as' => 'addmoney.stripe','uses' => 'StripeController@postPaymentWithStripe'));
+    Route::patch('update-profile', 'UsersController@update');
+  });
+  Route::get('locale/{locale}',  'LocaleController@locale')->name('locale');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Route::get('/admin', 'AdminController@login')->name('Admin Login');
-//Route::post('/admin/authenticate', 'AdminController@authenticate')->name('Admin Authentication');
-//Route::get('/admin/dashboard', 'AdminController@index')->name('Admin Dashboard');
-
-
-Route::get('profile',  'UsersController@profile')->name('profile');
-Route::get('edit-profile',  'UsersController@edit')->name('edit profile');
-Route::patch('update-profile', 'UsersController@update');
-//->name('update profile');
