8132facbf79fb506ee218816df19d47388e84c69
Haris Khan <haris.khan@gems.techverx.com>
2018-09-07 13:08:27 +0500

    Pictionary Game record table

---
+++ database/migrations/2018_09_06_064653_create_pictionary_game_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePictionaryGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictionary_game', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('score')->default(0);
+            $table->integer('question_count')->default(0);
+            $table->string('questions')->nullable();
+            $table->boolean('is_complete')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictionary_game');
+    }
+}
