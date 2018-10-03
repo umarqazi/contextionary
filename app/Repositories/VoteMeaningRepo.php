c0fdb2927586209cee65557fb07ebe1d63764109
Muhammad Adeel <muhammad.adeel@gems.techverx.com>
2018-10-03 12:40:15 +0500

    updated where clause in highest bids

--- app/Repositories/VoteMeaningRepo.php
+++ app/Repositories/VoteMeaningRepo.php
@@ -72,6 +72,7 @@ class VoteMeaningRepo
      * get highest votes
      */
     public function hightVotes($checkArray){
-        return $total=$this->getVotesMeaning($checkArray)->with($checkArray['type'])->where('vote', 1)->groupBy($checkArray['columnKey'])->select('vote_meanings.*', DB::raw('count(*) as total'))->limit(3)->orderBy('total', 'DESC')->get();
+        $data=['context_id'=>$checkArray['context_id'], 'phrase_id'=>$checkArray['phrase_id'], 'type'=>$checkArray['type']];
+        return $total=$this->getVotesMeaning($data)->with($checkArray['type'])->where('vote', 1)->groupBy($checkArray['columnKey'])->select('vote_meanings.*', DB::raw('count(*) as total'))->limit(3)->orderBy('total', 'DESC')->get();
     }
 }
