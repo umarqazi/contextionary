410f42b2c86c2f75ed2546df38eaa16d27b3bc93
Haris Khan <haris.khan@gems.techverx.com>
2018-10-12 17:16:50 +0500

    Cards on Admin Homepage

--- app/BiddingExpiry.php
+++ app/BiddingExpiry.php
@@ -6,6 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class BiddingExpiry extends Model
 {
+    /**
+     * @var string
+     */
     public $table='bidding_expiry';
+
+    /**
+     * @var bool
+     */
     public $timestamps=false;
+
+    /**
+     * @return mixed
+     */
+    public function countPharseInDefinePhase(){
+       return self::where('bid_type', env('MEANING'))->where('status',0)->count();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function countPharseInIllustrationPhase(){
+       return self::where('bid_type', env('ILLUSTRATE'))->where('status',0)->count();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function countPharseInTranslationPhase(){
+       return self::where('bid_type', env('TRANSLATE'))->where('status',0)->count();
+    }
 }
