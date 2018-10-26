21620fa628c7240a7c0d423d7f4401fa59682a10
Haris Khan <haris.khan@gems.techverx.com>
2018-10-26 20:59:56 +0500

    Changes in active plan blade

--- resources/views/user/user_plan/plan/active_plan.blade.php
+++ resources/views/user/user_plan/plan/active_plan.blade.php
@@ -22,7 +22,25 @@
 
         <div class="row mt-4">
             <div class="col-md-12">
-                <div class="planTitle">{!! t('Current plan') !!} <span>({!! t('remaining time') !!}: {!! $days !!} {{ t(str_plural('day', $days)) }})</span></div>
+                @if($activePlan)
+                    @if($activePlan->expiry_date == '')
+                        <div class="planTitle">
+                            {!! t('Current plan') !!} <span>({!! t('remaining time') !!}: {{t('Unlimited')}} {{ t('days') }})</span>
+                        </div>
+                    @elseif($activePlan->expiry_date <= carbon::now())
+                        <div class="planTitle">
+                            {!! t('Current plan') !!} <span>({!! t('Expired') !!})</span>
+                        </div>
+                    @else
+                        <div class="planTitle">
+                            {!! t('Current plan') !!} <span>({!! t('remaining time') !!}: {!! $days !!} {{ t(str_plural('day', $days)) }})</span>
+                        </div>
+                    @endif
+                @else
+                    <div class="planTitle">
+                        {!! t('Current plan') !!} <span>({!! t('remaining time') !!}: {!! $days !!} {{ t(str_plural('day', $days)) }})</span>
+                    </div>
+                @endif
             </div>
 
             <div class="col-lg-8">
@@ -55,15 +73,13 @@
                                     <li>Learning Center</li>
                                 @endif
                             </ul>
-
-                            <div class="md-form ml-4 mt-2">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="checkBox3">
-                                    <label class="custom-control-label" for="checkBox3">Activate auto renewel</label>
-                                </div>
-                            </div>
-                            @if($activePlan->expiry_date < carbon::now())
-                                <button class="orangeBtn mb-3">renew plan</button>
+                            @if($activePlan->expiry_date == '')
+                            @elseif($activePlan->expiry_date <= carbon::now())
+                                <a href="{!! lang_url('payment', ['id'=>$activePlan->package_id]) !!}" class="orangeBtn mb-3 mt-3">Renew Plan</a>
+                            @elseif($activePlan->expiry_date > carbon::now())
+                                @if(!$activePlan->sub)
+                                    <a href="{!! lang_url('autopay', ['id'=>$activePlan->package_id]) !!}" class="orangeBtn mb-3 mt-3">Auto Renewal</a>
+                                @endif
                             @endif
                         </div>
                         <div class="row card-div ">
@@ -88,7 +104,41 @@
                 @endif
                 <div class="planTitle">{!! t('other plans') !!}</div>
                 @foreach(Config::get('constant.packages') as $key=>$package)
-                    @if($key!=$activePlan->package_id)
+                    @if($activePlan)
+                        @if($key!=$activePlan->package_id)
+                            <div class="planBlock">
+                                <div class="img-holder">
+                                    <img src="{!! asset('assets/images/plan').$key.'.png' !!}">
+                                </div>
+                                <div class="planInfo">
+                                    <div class="activePlan"><i class="fa fa-certificate"></i> amount / month: ${!! Config::get('constant.plan_prices.'.$key) !!}</div>
+                                    <h2>Features</h2>
+                                    <ul class="features">
+                                        <li>Reading assistant<br>
+                                            <span>Context,</span>
+                                            <span>Keywords,</span>
+                                            <span>Definition,</span>
+                                            <span>Illustration,</span>
+                                            <span>Related words</span>
+                                            @if($key!=1)
+                                                <span>Export Results</span>
+                                            @endif
+                                            @if($key==2)
+                                                <span>File Upload</span>
+                                            @endif
+                                        </li>
+                                        <li>Glossary catalog</li>
+                                        <li>A game of context</li>
+                                        @if($key==2)
+                                            <li>Learning Center</li>
+                                        @endif
+                                    </ul>
+
+                                    <a href="{!! lang_url('payment', ['id'=>$key]) !!}" class="orangeBtn mb-3 mt-3">Purchase</a>
+                                </div>
+                            </div>
+                        @endif
+                    @else
                         <div class="planBlock">
                             <div class="img-holder">
                                 <img src="{!! asset('assets/images/plan').$key.'.png' !!}">
@@ -122,10 +172,7 @@
                         </div>
                     @endif
                 @endforeach
-
             </div>
-
         </div>
-
     </div>
 @endsection
