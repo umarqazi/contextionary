2cac08f2a844e903e945976e07610c038ea84b0b
Haris Khan <haris.khan@gems.techverx.com>
2018-10-26 20:49:14 +0500

    Plan migration

---
+++ database/migrations/2018_10_25_140320_create_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('plan_id');
+            $table->string('name');
+            $table->string('amount');
+            $table->string('currency');
+            $table->string('interval');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
