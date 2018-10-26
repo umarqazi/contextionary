2cc9a49a9a07a3be4d1c530a776f3a5ed1c1a6bb
Muhammad Adeel <muhammad.adeel@gems.techverx.com>
2018-10-26 14:11:18 +0500

    fixed some bugs on contributor side, some js validation required on add illustrator

--- resources/views/user/contributor/translation.blade.php
+++ resources/views/user/contributor/translation.blade.php
@@ -7,6 +7,6 @@
 <label class="customLabel">{!! t('Meaning Translation') !!}</label>
 {!! Form::textarea('translation', $data['translation']['translation'], ['maxlength'=>"2500",'id'=>'meaning-area','class'=>'enter-phrase' ,'placeholder'=>t('Translate Phrase')]) !!}
 <p class="text-right white-text"><span id="count">{!! t('Characters') !!} {!! ($data['translation']['translation'])? strlen($data['translation']['translation']):strlen(Input::old('translation')) !!}/2500</span></p>
-@if ($errors->has('illustrate'))
-    <div class="help-block"><strong>{{ $errors->first('illustrate') }}</strong></div>
+@if ($errors->has('translation'))
+    <div class="help-block"><strong>{{ $errors->first('translation') }}</strong></div>
 @endif
