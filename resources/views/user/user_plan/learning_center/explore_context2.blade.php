834730d4bb07e14f90c4bdb04f10b9f6852c5198
Haris Khan <haris.khan@gems.techverx.com>
2018-11-02 19:43:12 +0500

    Pagination links

--- resources/views/user/user_plan/learning_center/explore_context2.blade.php
+++ resources/views/user/user_plan/learning_center/explore_context2.blade.php
@@ -29,6 +29,15 @@
                         </div>
                     </div>
                 @endforeach
+                <div class="col-md-12 mt-4 text-center">
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            {{$phrases->links()}}
+                        </div>
+                        <div class="col-md-4"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
