18be00005d73ba8f7464ac90dc8bbeb692c891b3
Haris Khan <haris.khan@gems.techverx.com>
2018-11-02 19:22:33 +0500

    Switch Account disabled on guest

--- resources/views/layouts/main_header.blade.php
+++ resources/views/layouts/main_header.blade.php
@@ -64,10 +64,12 @@
               <a href="{!! lang_url('switchToUser') !!}" class="account"><i class="fa fa-angle-right"></i> {!! t('Switch to User Account') !!}</a>
             </div>
           @else
+            @if(!Auth::user()->hasRole(Config::get('constant.userRole.guest')))
             <img src="{!! asset('assets/images/switch-account-icon.png') !!}"> <span> <i class="fa fa-angle-down"></i></span>
             <div class="dropDown-block">
               <a href="{!! lang_url('switchToContributor') !!}" class="account"><i class="fa fa-angle-right"></i> {!! t('Switch to Contibutor Account') !!}</a>
             </div>
+            @endif
           @endif
         @endif
       </div>
