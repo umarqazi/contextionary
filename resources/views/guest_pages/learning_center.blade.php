b496dd7d6bd1aaf6e3cfb23108c7225e8f9b6b09
Haris Khan <haris.khan@gems.techverx.com>
2018-11-02 19:24:40 +0500

    Learning Center Blade

---
+++ resources/views/guest_pages/learning_center.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.secured_header')
+@section('title')
+    {!! t('Learning Center') !!}
+@stop
+@section('content')
+<div class="container-fluid contributorMain userLearningbg">
+    @include('layouts.flc_header')
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="learningModule">
+                <div class="wrapper">
+                    <div class="companyName">Learning module</div>
+                    <div class="actions-btn mt-5">
+                        <a href="{!! lang_route('explore-context') !!}" class="btn orangeBtn">Explore a context</a>
+                        <br>
+                        <a href="{!! lang_route('explore-word') !!}" class="btn orangeBtn blue-btn mt-2">Explore a Word</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
