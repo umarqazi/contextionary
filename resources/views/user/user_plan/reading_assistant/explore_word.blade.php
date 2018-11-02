4a5d4f9dc79159ae8aa3055a7b1defa70e307e11
Haris Khan <haris.khan@gems.techverx.com>
2018-11-02 19:14:15 +0500

    Explore Word

---
+++ resources/views/user/user_plan/reading_assistant/explore_word.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.secured_header')
+@section('title')
+    {!! t('Pictionary') !!}
+@stop
+@section('content')
+    <div class="container-fluid contributorMain userExploreWord userExploreContext2 resultPage">
+        @include('layouts.flc_header')
+        <div class="learningModule">
+            <div class="wrapper">
+                <div class="companyName">Explore A Word</div>
+                <div class="actions-btn mt-5">
+                    @include('user.user_plan.reading_assistant.context_search')
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
