25629fe5a0efc59ca4345c6258aa7a47c1ddef53
Haris Khan <haris.khan@gems.techverx.com>
2018-11-06 18:47:09 +0500

    Redeem Admin Controller

---
+++ app/Admin/Controllers/RedeemController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Coin;
+use Illuminate\Support\Facades\Storage;
+use \Illuminate\Database\Eloquent\Model;
+use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Illuminate\Http\Request;
+
+class CoinsController extends Controller
+{
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header(trans('Coins Deals'));
+            $content->description(trans('Coins Deals List'));
+            $content->body($this->grid()->render());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     *
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header('Coins Deals');
+            $content->description('Edit Coins Deal');
+            $content->body($this->form($id)->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('Coins Deals');
+            $content->description('Add coins deals');
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(Coin::class, function (Grid $grid) {
+            $grid->id('ID')->sortable();
+            $grid->disableExport();
+            $grid->option('useWidth', true);
+            $grid->price()->sortable();
+            $grid->coins()->sortable();
+            $grid->column('created_at','Created at')->sortable();
+            $grid->column('updated_at','Last Updated at')->sortable();
+            $grid->filter(function ($filter){
+                $filter->like('price');
+                $filter->like('coins');
+            });
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+                $action = "".$actions->getResource()."/".$actions->getKey()."";
+                $actions->prepend('<a href="'.$action.'"><i class="fa fa-eye"></i></a>');
+            });
+            $grid->tools(function (Grid\Tools $tools) {
+                $tools->batch(function (Grid\Tools\BatchActions $actions) {
+                    $actions->disableDelete();
+                });
+            });
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @param $id
+     * @return Form
+     */
+    public function form($id = null)
+    {
+        return Admin::form(Coin::class, function (Form $form) use ($id) {
+            $form->display('id', 'ID');
+            $form->text('price', trans('Price'))->rules('required')->placeholder('Enter Price...');
+            $form->text('coins', trans('Coins'))->rules('required')->placeholder('Enter Coins...');
+            $form->saved(function (Form $form) use ($id) {
+                if($id){
+                    admin_toastr(trans('Updated successfully!'));
+                }else{
+                    admin_toastr(trans('New Coins Deal created successfully!'));
+                }
+                return redirect(admin_base_path('auth/coins-deals'));
+            });
+        });
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return Content
+     */
+    public function show($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header('Coins Deals');
+            $content->description('View coins deal');
+            $content->body($this->form($id)->view($id));
+        });
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param int $id
+     *
+     * @return Form
+     */
+    public function update($id)
+    {
+        return $this->form($id)->update($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $package = Coin::find($id);
+        if ($package->delete()) {
+            admin_toastr(trans('admin.delete_succeeded'));
+            return response()->json([
+                'status'  => true,
+                'message' => trans('admin.delete_succeeded'),
+            ]);
+        } else {
+            return response()->json([
+                'status'  => false,
+                'message' => trans('admin.delete_failed'),
+            ]);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return mixed
+     */
+    public function store()
+    {
+        return $this->form()->store();
+    }
+}
