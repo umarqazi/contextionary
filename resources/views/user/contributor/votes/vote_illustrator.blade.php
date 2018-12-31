42e4ad09e19898224aa604c46370b27c227191b4
Muhammad Adeel <muhammad.adeel@gems.techverx.com>
2018-12-31 13:56:34 +0500

    fixed

--- resources/views/user/contributor/votes/vote_illustrator.blade.php
+++ resources/views/user/contributor/votes/vote_illustrator.blade.php
@@ -34,7 +34,7 @@
                                             <input type="radio" class="illustrator-checkbox" name="illustrator" @if(old('illustrator')==$illustrator['id']) checked @endif value="{!! $illustrator['id'] !!}">
                                         <img src="{!! asset('storage') !!}/{!! $illustrator['illustrator'] !!}" class="main-img">
                                         <div class="mask"></div>
-                                        <a class="fancybox" href="{!! asset('storage') !!}/{!! $illustrator['illustrator'] !!}"><i class="fa fa-search"></i></a></label>
+                                        <a class="fancybox-abc" href="{!! asset('storage') !!}/{!! $illustrator['illustrator'] !!}"><i class="fa fa-search"></i></a></label>
                                     </div>
                                 </div>
                             @endforeach
