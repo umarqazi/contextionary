fe093d533fc812ac92d0678268b6a7abd7a23622
Muhammad Adeel <muhammad.adeel@gems.techverx.com>
2019-01-01 16:18:55 +0500

    home slider separated from dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,45 +9,87 @@
     <div class="row">
       <div class="col-md-12 text-center">
         <div class="companyName mt-5">{!! t('Contextionary') !!}</div>
-        <div class="contributorSlider">
-          <div>
-            <div class="sliderBlock">
-              <h2>{!! t('Welcome') !!} {!! Auth::user()->first_name !!}</h2>
-              <p>{!! t('Dashboard Slide 1 Text')!!}</p>
+        @if(Auth::user()->hasRole(Config::get('constant.contributorRole')))
+          <div class="contributorSlider">
+            <div>
+              <div class="sliderBlock">
+                <h2>{!! t('Welcome') !!} {!! Auth::user()->first_name !!}</h2>
+                <p>{!! t('Dashboard Slide 1 Text')!!}</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <div class="sliderBlock">
-              <h2>{!! t('Contribute') !!}</h2>
-              <p>{!! t('Dashboard Slide 2 Text')!!}</p>
-              <ul class="iconsListing">
-                <li>
-                  <div class="circle">
-                    <img src="{!! asset('assets/images/slider-icon1.png') !!}">
-                  </div>
-                </li>
+            <div>
+              <div class="sliderBlock">
+                <h2>{!! t('Contribute') !!}</h2>
+                <p>{!! t('Dashboard Slide 2 Text')!!}</p>
+                <ul class="iconsListing">
+                  <li>
+                    <div class="circle">
+                      <img src="{!! asset('assets/images/slider-icon1.png') !!}">
+                    </div>
+                  </li>
 
-                <li>
-                  <div class="circle">
-                    <img src="{!! asset('assets/images/slider-icon2.png') !!}">
-                  </div>
-                </li>
+                  <li>
+                    <div class="circle">
+                      <img src="{!! asset('assets/images/slider-icon2.png') !!}">
+                    </div>
+                  </li>
 
-                <li>
-                  <div class="circle">
-                    <img src="{!! asset('assets/images/slider-icon3.png') !!}">
-                  </div>
-                </li>
-              </ul>
+                  <li>
+                    <div class="circle">
+                      <img src="{!! asset('assets/images/slider-icon3.png') !!}">
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div class="sliderBlock">
+                <h2>{!! t('Welcome') !!} {!! Auth::user()->first_name !!}</h2>
+                <p>{!! t('Dashboard Slide 3 Text')!!}</p>
+              </div>
             </div>
           </div>
-          <div>
-            <div class="sliderBlock">
-              <h2>{!! t('Welcome') !!} {!! Auth::user()->first_name !!}</h2>
-              <p>{!! t('Dashboard Slide 2 Text')!!}</p>
+        @else
+          <div class="contributorSlider">
+            <div>
+              <div class="sliderBlock">
+                <h2>{!! t('Welcome') !!} {!! Auth::user()->first_name !!}</h2>
+                <p>{!! t('User Dashboard Slide 1 Text')!!}</p>
+              </div>
+            </div>
+            <div>
+              <div class="sliderBlock">
+                <h2>{!! t('Contribute') !!}</h2>
+                <p>{!! t('User  Dashboard Slide 2 Text')!!}</p>
+                <ul class="iconsListing">
+                  <li>
+                    <div class="circle">
+                      <img src="{!! asset('assets/images/slider-icon1.png') !!}">
+                    </div>
+                  </li>
+
+                  <li>
+                    <div class="circle">
+                      <img src="{!! asset('assets/images/slider-icon2.png') !!}">
+                    </div>
+                  </li>
+
+                  <li>
+                    <div class="circle">
+                      <img src="{!! asset('assets/images/slider-icon3.png') !!}">
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div class="sliderBlock">
+                <h2>{!! t('Welcome') !!} {!! Auth::user()->first_name !!}</h2>
+                <p>{!! t('User  Dashboard Slide 3 Text')!!}</p>
+              </div>
             </div>
           </div>
-        </div>
+        @endif
       </div>
     </div>
   </div>
