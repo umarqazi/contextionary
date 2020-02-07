a23736cbcc2f5e849a6a88a4d5ee1294fc9548ca
faisal-techverx <faisal.raza@gems.techverx.com>
2020-02-07 20:16:07 +0500

    Add new column manual shuffle to context marathon

--- app/ContextMarathon.php
+++ app/ContextMarathon.php
@@ -9,7 +9,8 @@ class ContextMarathon extends Model
     protected $connection = 'pgsql';
     protected $table = 'context_marathon';
     protected $primaryKey = 'id';
-    protected $fillable = ['context_id', 'phrase_id', 'bucket', 'hint_1', 'hint_2', 'hint_3', 'hint_4', 'hint_5', 'hint_6'];
+    protected $fillable = ['context_id', 'phrase_id', 'bucket', 'hint_1', 'hint_2', 'hint_3', 'hint_4', 'hint_5', 'hint_6', 'manual_shuffle'];
+    public $timestamps = false;
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
