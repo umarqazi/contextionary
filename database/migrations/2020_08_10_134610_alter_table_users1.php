551003421d10218ce726ad3213068b1f8795111a
faisal-techverx <faisal.raza@gems.techverx.com>
2020-08-13 13:22:27 +0500

    Set max unlocked context default value to 1

--- database/migrations/2020_08_10_134610_alter_table_users1.php
+++ database/migrations/2020_08_10_134610_alter_table_users1.php
@@ -14,7 +14,7 @@ class AlterTableUsers1 extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->integer('max_unlocked_context')->nullable();
+            $table->integer('max_unlocked_context')->default(1);
             $table->string('result_hint_index')->nullable();
             $table->string('current_letter_text')->nullable();
             $table->string('username')->after('email')->unique()->nullable();
@@ -29,7 +29,6 @@ class AlterTableUsers1 extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('coffee_break_count');
             $table->dropColumn('max_unlocked_context');
             $table->dropColumn('result_hint_index');
             $table->dropColumn('current_letter_text');
